25b2b4343e8f4bfcaf4bae3bcc492ec77adb2211
Bent <horstikugel@gmail.com>
2020-01-21 02:15:31 +0100

    formatting

--- lib/sequence.dart
+++ lib/sequence.dart
@@ -4,6 +4,55 @@ import 'package:meta/meta.dart';
 
 typedef VoidCallback = void Function();
 
+/// Basically a Timer that hasn't started yet.
+class Event implements Comparable {
+  Duration _offset;
+  /// Callback that should be fired when this [Event] happens.
+  VoidCallback callback;
+
+  final Set<Sequence> _registeredIn = {};
+
+  /// Create an [Event] offset by [offset] from [Point 0].
+  Event([offset = const Duration(), this.callback]) {
+    _offset = offset;
+  }
+
+  /// The [Duration] to [Point 0]
+  Duration get offset => _offset;
+  set offset(Duration offset) {
+    _offset = offset;
+    registeredIn.forEach((sequence) => sequence._updateTimings());
+  }
+
+  /// All of the [Sequence]s this [Event] is part of.
+  List<Sequence> get registeredIn => List.unmodifiable(_registeredIn);
+
+  /// Sorts by [offset], negative [Duration]s are smaller.
+  @override
+  int compareTo(dynamic other) => offset.compareTo(other.offset);
+
+  /// Internal thing that links this to a [Sequence].
+  /// This is done so that the [Duration] can be updated
+  /// and the [Sequence]s reflect that.
+  ///
+  /// You don't need this either.
+  @protected
+  void registerSequence(Sequence sequence) =>
+      {if (sequence.events.contains(this)) _registeredIn.add(sequence)};
+
+  /// Returns a new [Timer] that will fire [callback] in [offset].
+  ///
+  /// [offsetFurther] offsets the [offset], but it's per default 0 anyways.
+  /// [callback] is the callback being called when the [Timer] runs out.
+  Timer toTimer([Duration offsetFurther = const Duration()]) =>
+      Timer(offset + offsetFurther, callback);
+
+  /// Don't touch XD.
+  @protected
+  void unregisterSequence(Sequence sequence) =>
+      {if (!sequence.events.contains(this)) _registeredIn.remove(sequence)};
+}
+
 /// TODO write tests lol
 /// TODO Make Events mutable again and have the Sequence update on change.
 ///   this might actually work though haha
@@ -21,31 +70,15 @@ typedef VoidCallback = void Function();
 /// You can use [call()] to play the sequence in a loop.
 /// Negative [Duration]s mean that the [Event] will played before [Time 0].
 class Sequence {
-  /// Stores the [Event]s that will be run.
-  List<Event> get events => List.unmodifiable(_events);
   final List<Event> _events = [];
-
-  /// Returns how long the [Sequence] goes before [Point 0].
-  /// Negative if there aren't [Event]s before [Point 0].
-  Duration get startOffset => _startOffset;
   Duration _startOffset;
 
-  /// The length of one run through the [Sequence].
-  Duration get length => _length;
   Duration _length;
-
-  /// The amount of times the [Sequence] has been repeated
-  /// since the last time it has been cancelled with [cancel()].
-  int get ticks => _driver != null ? _driver.tick : 0;
-
   /// This [Timer] is responsible for repeating the sequence.
   Timer _driver = null;
 
   /// When the [Sequence] [isRunning] this holds all of the [Timer] instances.
   final List<Timer> _timers = [];
-
-  /// Returns whether or not a sequence is currently playing.
-  bool get isRunning => _isRunning;
   bool _isRunning = false;
 
   /// Construct a [Sequence] from a [List] of [Event]s
@@ -58,57 +91,28 @@ class Sequence {
   /// Create an empty [Sequence].
   Sequence.empty();
 
+  /// Stores the [Event]s that will be run.
+  List<Event> get events => List.unmodifiable(_events);
+
+  /// Returns whether or not a sequence is currently playing.
+  bool get isRunning => _isRunning;
+  /// The length of one run through the [Sequence].
+  Duration get length => _length;
+
+  /// Returns how long the [Sequence] goes before [Point 0].
+  /// Negative if there aren't [Event]s before [Point 0].
+  Duration get startOffset => _startOffset;
+
+  /// The amount of times the [Sequence] has been repeated
+  /// since the last time it has been cancelled with [cancel()].
+  int get ticks => _driver != null ? _driver.tick : 0;
+
   /// Adds a single [Event] to the [Sequence].
   void add(Event event) {
     _events.add(event);
     event.registerSequence(this);
     _updateTimings();
   }
-
-  /// Removes a single [Event] from the [Sequence].
-  void remove(Event event) {
-    _events.remove(event);
-    event.unregisterSequence(this);
-    _updateTimings();
-  }
-
-  /// Recalculates the in- and outpoints.
-  ///
-  /// There is no need for you to touch this.
-  void _updateTimings() {
-    _events.sort();
-    _startOffset = -_events.first.offset;
-    _length = _events.last.offset + _startOffset;
-  }
-
-  /// Cancels any ongoing [Timer]s and stops the [Sequence] from playing.
-  void cancel() {
-    _driver?.cancel();
-    _timers?.forEach((timer) => timer.cancel());
-    _isRunning = false;
-    _timers.clear();
-    _driver = null;
-  }
-
-  /// Makes this removable by GC
-  ///
-  /// Cancels any ongoing [Timer]s,
-  /// stops the [Sequence] and removes all [Event]s.
-  void deactivate() {
-    cancel();
-    var tempCopies = _events.toList(growable: false);
-    _events.clear();
-    tempCopies.forEach((event) => event.unregisterSequence(this));
-  }
-
-  /// Starts this [Sequence],
-  ///
-  /// so that at [startPoint] an [Event] with 0 [offset] would be played.
-  /// This is a wrapper for [call()].
-  bool startAt(
-          {@required DateTime startPoint,
-          Duration period = const Duration()}) =>
-      call(startIn: DateTime.now().difference(startPoint), period: period);
 
   /// Starts the [Sequence],
   ///
@@ -154,16 +158,52 @@ class Sequence {
           if (isPeriodic) {
             _driver = Timer.periodic(period, _startSequence);
             _isRunning = true;
-          } 
+          }
         });
       });
     } // Shut up, Dart Analyzer
+  }
+
+  /// Cancels any ongoing [Timer]s and stops the [Sequence] from playing.
+  void cancel() {
+    _driver?.cancel();
+    _timers?.forEach((timer) => timer.cancel());
+    _isRunning = false;
+    _timers.clear();
+    _driver = null;
   }
 
   /// Cleanup by removing inactive [Timer]s from [_timers].
   void cleanup() async {
     _timers.removeWhere((timer) => !timer.isActive);
   }
+
+  /// Makes this removable by GC
+  ///
+  /// Cancels any ongoing [Timer]s,
+  /// stops the [Sequence] and removes all [Event]s.
+  void deactivate() {
+    cancel();
+    var tempCopies = _events.toList(growable: false);
+    _events.clear();
+    tempCopies.forEach((event) => event.unregisterSequence(this));
+  }
+
+  /// Removes a single [Event] from the [Sequence].
+  void remove(Event event) {
+    _events.remove(event);
+    event.unregisterSequence(this);
+    _updateTimings();
+  }
+
+  /// Starts this [Sequence],
+  ///
+  /// so that at [startPoint] an [Event] with 0 [offset] would be played.
+  /// This is a wrapper for [call()].
+  bool startAt(
+          {@required DateTime startPoint,
+          Duration period = const Duration()}) =>
+      call(startIn: DateTime.now().difference(startPoint), period: period);
 
   /// Find the [Event] that should be started next, based on [startIn].
   int _nextEventIndex(Duration startIn) =>
@@ -197,53 +237,13 @@ class Sequence {
     }
     cleanup();
   }
-}
 
-/// Basically a Timer that hasn't started yet.
-class Event implements Comparable {
-  /// The [Duration] to [Point 0]
-  Duration get offset => _offset;
-  set offset(Duration offset) {
-    _offset = offset;
-    registeredIn.forEach((sequence) => sequence._updateTimings());
-  }
-
-  Duration _offset;
-
-  /// Callback that should be fired when this [Event] happens.
-  VoidCallback callback;
-
-  /// All of the [Sequence]s this [Event] is part of.
-  List<Sequence> get registeredIn => List.unmodifiable(_registeredIn);
-  final Set<Sequence> _registeredIn = {};
-
-  /// Internal thing that links this to a [Sequence].
-  /// This is done so that the [Duration] can be updated
-  /// and the [Sequence]s reflect that.
+  /// Recalculates the in- and outpoints.
   ///
-  /// You don't need this either.
-  @protected
-  void registerSequence(Sequence sequence) =>
-      {if (sequence.events.contains(this)) _registeredIn.add(sequence)};
-
-  /// Don't touch XD.
-  @protected
-  void unregisterSequence(Sequence sequence) =>
-      {if (!sequence.events.contains(this)) _registeredIn.remove(sequence)};
-
-  /// Create an [Event] offset by [offset] from [Point 0].
-  Event([offset = const Duration(), this.callback]) {
-    _offset = offset;
+  /// There is no need for you to touch this.
+  void _updateTimings() {
+    _events.sort();
+    _startOffset = -_events.first.offset;
+    _length = _events.last.offset + _startOffset;
   }
-
-  /// Returns a new [Timer] that will fire [callback] in [offset].
-  ///
-  /// [offsetFurther] offsets the [offset], but it's per default 0 anyways.
-  /// [callback] is the callback being called when the [Timer] runs out.
-  Timer toTimer([Duration offsetFurther = const Duration()]) =>
-      Timer(offset + offsetFurther, callback);
-
-  /// Sorts by [offset], negative [Duration]s are smaller.
-  @override
-  int compareTo(dynamic other) => offset.compareTo(other.offset);
 }
